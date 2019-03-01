36f0f740ab7bc27862c659586eccb643f21e3675
Vasilkou Dzmitry <vasilkou@RFERL_BEL.local>
2019-03-01 14:47:46 +0300

    feat: add function body

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let result = 0;
+preferences.forEach(function(item, i, arr){
+  if(i === preferences[preferences[preferences[i]-1]-1]-1){
+    result++;	
+  }
+})
+
+return Math.trunc(result / 3);
 };
